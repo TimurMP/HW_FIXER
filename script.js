30a830a081da2070255646d71f8131c0134a9eb0
TimurMP <titimurmamyshev@gmail.commur>
2023-01-31 00:12:03 +0200

    1.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = 'jBbJxkkiuvm0jgNETyTx0DCqYe8NGsMA';
-const url = `https://api.apilayer.com/fixer/convertttt?to=${to.value}&from=${from.value}&amount=${amount.value}`;
+// const url = `https://api.apilayer.com/fixer/convertttt?to=${to.value}&from=${from.value}&amount=${amount.value}`;
 
 let headers = {
     apikey: apiKey,
@@ -7,9 +7,7 @@ let headers = {
 }
 
 let reqOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: headers
+    method: 'GET', redirect: 'follow', headers: headers
 }
 
 
@@ -25,27 +23,24 @@ convertB.onclick = e => {
             }
         })
         .then(result => {
-                if (result.success) {
-                    clearStats();
-                    const divStats = document.createElement('div')
-                    const h1 = document.createElement('h1');
-                    h1.appendChild(document.createTextNode(`Result: ${result.result}`))
-                    divStats.appendChild(h1);
-                    if (stats.firstElementChild) {
-                        stats.replaceChild(divStats, stats.firstElementChild);
-                    } else {
-                        stats.appendChild(divStats);
-                    }
-                    console.log(result)
-
+            if (result.success) {
+                clearStats();
+                const divStats = document.createElement('div')
+                const h1 = document.createElement('h1');
+                h1.appendChild(document.createTextNode(`Result: ${result.result}`))
+                divStats.appendChild(h1);
+                if (stats.firstElementChild) {
+                    stats.replaceChild(divStats, stats.firstElementChild);
                 } else {
-                    console.log(result)
-                    throw new Error(result.error.info)
+                    stats.appendChild(divStats);
                 }
+                console.log(result)
 
-
+            } else {
+                console.log(result)
+                throw new Error(result.error.info)
             }
-        )
+        })
         .catch(e => {
             clearStats();
             console.log(e);
